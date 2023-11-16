a8cd8745f37f4aefe22fa44421c95ce2635adf29
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-11-16 16:23:39 -0300

    Script de conexão

---
+++ inc/conecta.php
@@ -0,0 +1,29 @@
+<?php
+/* Script de conexão ao servidor
+de Banco de Dados */
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "microblog_tiago";
+
+/* Usando a função mysqli_connect para conectar
+ao servidor de banco de dados */
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+/* Definindo o charset da conexão também como utf8 */
+mysqli_set_charset($conexao, "utf8");
+
+/* Verificação da conexão */
+
+// Se NÃO FOR POSSÍVEL realizar a conexão
+if( !$conexao ){
+    // PARE a aplicação e mostre uma mensagem de erro
+    die("Deu ruim: ".mysqli_connect_error());
+} else {
+    // Senão, a conexão foi feita com sucesso!
+    echo "Beleza, conectado!";
+}
+
+
+
+
