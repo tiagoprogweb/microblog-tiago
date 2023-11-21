a946f19fa411fb24fcb1a56b9ccae0f05050f406
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-11-21 16:51:40 -0300

    Update usuario-atualiza.php

--- admin/usuario-atualiza.php
+++ admin/usuario-atualiza.php
@@ -6,10 +6,8 @@ require_once "../inc/cabecalho-admin.php";
 $id = $_GET['id']; 
 
 // Chamando a função e guardando o retorno dela
-$dados = lerUmUsuario($conexao, $id);
+$usuario = lerUmUsuario($conexao, $id);
 ?>
-<pre> <?=var_dump($dados)?> </pre>
-
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
@@ -22,12 +20,12 @@ $dados = lerUmUsuario($conexao, $id);
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" required>
+				<input value="<?=$usuario['nome']?>" class="form-control" type="text" id="nome" name="nome" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input class="form-control" type="email" id="email" name="email" required>
+				<input value="<?=$usuario['email']?>" class="form-control" type="email" id="email" name="email" required>
 			</div>
 
 			<div class="mb-3">
@@ -39,8 +37,17 @@ $dados = lerUmUsuario($conexao, $id);
 				<label class="form-label" for="tipo">Tipo:</label>
 				<select class="form-select" name="tipo" id="tipo" required>
 					<option value=""></option>
-					<option value="editor">Editor</option>
-					<option value="admin">Administrador</option>
+
+
+					<option 
+					<?php if($usuario["tipo"] === "editor") echo "selected"; ?>
+					value="editor">Editor</option>
+
+					<option 
+					<?php if($usuario["tipo"] === "admin") echo "selected"; ?>
+					value="admin">Administrador</option>
+
+
 				</select>
 			</div>
 			
