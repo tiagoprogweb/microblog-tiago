2060a265c987cdd192fc2085b7368ea508c6b434
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-11-23 14:58:44 -0300

    Create funcoes-noticias.php

---
+++ inc/funcoes-noticias.php
@@ -0,0 +1,90 @@
+<?php
+require "conecta.php";
+
+/* Usada em noticia-insere.php */
+function inserirNoticia($conexao){
+    
+
+    // mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+} // fim inserirNoticia
+
+
+/* Usada em noticia-insere.php e noticia-atualiza.php */
+function upload(){
+    
+} // fim upload
+
+
+/* Usada em noticias.php */
+function lerNoticias($conexao){
+    
+
+    // mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+} // fim lerNoticias
+
+
+/* Usada em noticias.php e páginas da área pública */
+function formataData(){    
+    
+} // fim formataData
+
+
+/* Usada em noticia-atualiza.php */
+function lerUmaNoticia($conexao){
+    
+
+    // mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+} // fim lerUmaNoticia
+
+
+/* Usada em noticia-atualiza.php */
+function atualizarNoticia($conexao){
+    
+
+    // mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+} // fim atualizarNoticia
+
+
+/* Usada em noticia-exclui.php */
+function excluirNoticia($conexao){
+
+
+    // mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+} // fim excluirNoticia
+
+
+
+/* ******************************************************* */
+
+
+/* Funções usadas nas páginas da área pública */
+
+/* Usada em index.php */
+function lerTodasAsNoticias($conexao){
+    
+
+    // mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+} // fim lerTodasAsNoticias
+
+
+/* Usada em noticia.php */
+function lerDetalhes($conexao){
+    
+
+    // mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+} // fim lerDetalhes
+
+
+/* Usada em resultados.php */
+function busca($conexao){
+    
+    // mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+} // fim busca
