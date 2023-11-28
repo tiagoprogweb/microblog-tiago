005b6fd2324c11ee2a84acf16703db363f7ac5b9
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-11-28 17:26:45 -0300

    Update noticia-atualiza.php

--- admin/noticia-atualiza.php
+++ admin/noticia-atualiza.php
@@ -12,7 +12,30 @@ $tipoUsuario = $_SESSION['tipo'];
 /* Chamamos a função e passamos os parâmetros */
 $noticia = lerUmaNoticia($conexao, $idNoticia, $idUsuario, $tipoUsuario);
 
+if(isset($_POST['atualizar'])){
+    $titulo = $_POST['titulo'];
+    $texto = $_POST['texto'];
+    $resumo = $_POST['resumo'];
 
+    /* Lógica/Algoritmo para a imagem */
+
+    /* Se o campo imagem estiver vazio, então
+    significa que o usuário NÃO QUER TROCAR A IMAGEM.
+    Ou seja, o sistema vai manter a imagem existente. */
+    if( empty($_FILES['imagem']['name']) ){
+        $imagem = $_POST['imagem-existente'];
+    } else {
+        /* Caso contrário, então pegamos a referência do novo
+        arquivo (nome e extensão) e fazemos o processo de upload. */
+        $imagem = $_FILES['imagem']['name'];
+        upload($_FILES['imagem']);
+    }
+
+    atualizarNoticia($conexao, $titulo, $texto, $resumo, 
+                $imagem, $idNoticia, $idUsuario, $tipoUsuario);
+
+    header("location:noticias.php");
+} // fim if isset
 ?>
 
 
