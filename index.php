ca28318caa767e24987652209e369edcb75b1ecc
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-11-29 17:27:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,28 +4,29 @@ require "inc/cabecalho.php";
 
 // Chamando a função para carregar os dados de todas notícias
 $listaDeNoticias = lerTodasAsNoticias($conexao);
-
-echo "<pre>";
-var_dump($listaDeNoticias);
-echo "</pre>";
 ?>  
-
 
 <div class="row my-1 mx-md-n1">
 
+<?php foreach( $listaDeNoticias as $noticia ){ ?>
         <!-- INÍCIO Card -->
 		<div class="col-md-6 my-1 px-md-1">
             <article class="card shadow-sm h-100">
-                <a href="noticia.php" class="card-link">
-                    <img src="imagens/abstrato.jpg" class="card-img-top" alt="">
+                <a href="noticia.php?id=<?=$noticia['id']?>" class="card-link">
+                    <img src="imagens/<?=$noticia['imagem']?>" class="card-img-top" alt="">
                     <div class="card-body">
-                        <h3 class="fs-4 card-title">Título da notícia...</h3>
-                        <p class="card-text">Resumo da notícia.</p>
+                        <h3 class="fs-4 card-title">
+                            <?=$noticia['titulo']?>
+                        </h3>
+                        <p class="card-text">
+                            <?=$noticia['resumo']?>
+                        </p>
                     </div>
                 </a>
             </article>
 		</div>
 		<!-- FIM Card -->
+<?php } ?>
 
 </div>        
 
