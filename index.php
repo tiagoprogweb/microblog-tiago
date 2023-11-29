bf38d4bac9ec183ec1ee269167045f4b4c920c47
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-11-29 16:26:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require "inc/cabecalho.php";
 
 <div class="row my-1 mx-md-n1">
 
-    <!-- INÍCIO Card -->
+        <!-- INÍCIO Card -->
 		<div class="col-md-6 my-1 px-md-1">
             <article class="card shadow-sm h-100">
                 <a href="noticia.php" class="card-link">
@@ -19,19 +19,6 @@ require "inc/cabecalho.php";
 		</div>
 		<!-- FIM Card -->
 
-        <!-- INÍCIO Card -->
-		<div class="col-md-6 my-1 px-md-1">
-            <article class="card shadow-sm h-100">
-                <a href="noticia.php" class="card-link">
-                    <img src="imagens/conversa.jpg" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h3 class="fs-4 card-title">Título da notícia...</h3>
-                        <p class="card-text">Resumo da notícia.</p>
-                    </div>
-                </a>
-            </article>
-		</div>
-		<!-- FIM Card -->
 </div>        
 
 <?php 
